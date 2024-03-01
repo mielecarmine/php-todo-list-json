73a6ff10965942004a5427fb5a4c700d67ac816e
mielecarmine <mielecarmine993@gmail.com>
2024-03-01 17:49:43 +0100

    feat: get list from json add: new task feature

--- backend/api/get-list.php
+++ backend/api/get-list.php
@@ -1,1 +1,5 @@
-p
+<?php 
+
+$json_list_content = file_get_contents('../data/todolist.json');
+header('Content-Type: application/json');
+echo $json_list_content;
--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,42 @@ const { createApp } = Vue;
 
 const App = createApp({
   data() {
-    return {};
+    return {
+      todoList: [],
+      newTask: {
+        todo: "",
+        done: false,
+      },
+    };
   },
-}).mount("#app");
+
+  methods: {
+    fetchToDoList() {
+      axios.get("../backend/api/get-list.php").then((res) => {
+        console.log(res.data);
+        this.todoList = res.data;
+      });
+    },
+
+    addTask() {
+      const task = this.newTask;
+      this.newTask.todo = "";
+
+      const data = {
+        params: {
+          newTask,
+        },
+      };
+
+      const params = {
+        headers: { "Content-Type": "multipart/form-data" },
+      };
+      axios.post("../backend/api/get-list.php", data, params);
+    },
+  },
+
+  mounted() {
+    this.fetchToDoList();
+  },
+});
+App.mount("#app");
